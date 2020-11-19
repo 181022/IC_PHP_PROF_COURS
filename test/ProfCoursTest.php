086eb920049a8643e767a084c48dc7a7a9e538c7
Diego Mauroy <diego.mauroy@student.umons.ac.be>
2020-11-19 15:25:21 +0100

    test add cours

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -174,7 +174,7 @@ class ProfCoursTest extends TestCase
         foreach (self::$cours_a as $cours) {
             $cours->add($conn);
         }
-        $expected = count(self::$prof_a);
+        $expected = count(self::$cours_a);
         $num_records = Cours::count($conn);
         $this->assertEquals($expected, $num_records, "Enregistrement des cours ...\n");
         $this->assertCount($num_records, self::$cours_a, "Enregistrement des cours ...\n");
