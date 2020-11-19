713fea1a074590939d96bbfd1a2d600793f7ce0f
Diego Mauroy <diego.mauroy@student.umons.ac.be>
2020-11-19 15:49:49 +0100

    test testupdateone corr4

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -313,10 +313,10 @@ class ProfCoursTest extends TestCase
         $this->assertTrue($val, "Update du prof num $idProf ...\n");
 
         // Cours
-        $cours = new Cours($this->intitule, $this->duree);
-        $val = $cours->updateOne($conn, $idCours, $idProf);
+        $cours = new Cours($this->intitule, $this->duree, $idProf);
+        $val = $cours->updateOne($conn, $idCours);
         $expected_cours_str = $cours->__toString();
-        $record_cours = Cours::printOne($conn, $idCours, $idProf);
+        $record_cours = Cours::printOne($conn, $idCours);
         $this->assertEquals($expected_cours_str, $record_cours->__toString(), "Update du cours $idCours ...\n");
         $this->assertTrue($val, "Update du cours num $idCours ...\n");
         
