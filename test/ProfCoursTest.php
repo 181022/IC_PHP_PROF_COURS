4f46dd3eba5162892e8989c6c41f25347403ef3a
Diego Mauroy <diego.mauroy@student.umons.ac.be>
2020-11-19 15:35:33 +0100

    test testprintone correction

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -259,7 +259,7 @@ class ProfCoursTest extends TestCase
         print "########## - 1e COURS EN BASE - ########## \n";
         print $cours_str."\n";
         print "################################################################\n\n";
-        $expected = self::$prof_a[0]->__toString();
+        $expected = self::$cours_a[0]->__toString();
         $this->assertEquals($expected, $cours_str, "Cours \n");
         
 
